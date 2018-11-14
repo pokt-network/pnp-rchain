c20970560e7b283689c983098a7070c6e08b151a
Pabel Nunez Landestoy <pabelnunezl@gmail.com>
2018-11-14 09:21:00 -0400

    Multiple fixes and changes

--- test/index.js
+++ test/index.js
@@ -1,48 +1,44 @@
 
 var assert = require('assert'),
-    rchainPnp = require('../index.js'),
+    rchainPnp = require('../lib/index.js'),
     packageData = require('../package.json'),
-    rchainApi = require('rchain-api'),
-    accounts = [],
-    rNode = 'localhost',
+    RNodeAPI = require('rchain-api'),
+    grpc = require('grpc'),
+    rNode_host = 'localhost',
     testRpcPort = 40401,
-    networkId = 5777,
+    networkId = 7777,
     web3 = null,
     rchainPnpOpts = {
-      rchain_node: rNode,
+      rchain_node: rNode_host,
       network_id: networkId
     };
 
-before(function(done) {
-  rchainApi = RNode(grpc, { host: rNode, port: testRpcPort });
-  // rchainApi.eth.getAccounts(function(error, web3Accounts) {
-  //   accounts = web3Accounts;
-  //   done();
-  // });
-});
+// before(function(done) {
+//   RNodeAPI = RNodeAPI.RNode(grpc, { host: rNode_host, port: testRpcPort });
+// });
 
 describe('Plugin Configuration', function() {
   it('should parse the necessary attributes', function() {
     var opts = {rchain_node: 'http://localhost:40401', network_id: '40401'},
-        parsedOpts = rchainPnpparseOpts(opts);
+        parsedOpts = rchainPnp.parseOpts(opts);
 
     assert.equal(opts.rchain_node, parsedOpts.rNode);
     assert.equal(opts.network_id, parsedOpts.networkId);
   });
 
   it('should have defaults', function() {
-    var parsedOpts = rchainPnpparseOpts({});
+    var parsedOpts = rchainPnp.parseOpts({});
 
-    assert.equal(parsedOpts.rNode, rchainPnpDEFAULT_RCHAIN_NODE);
-    assert.equal(parsedOpts.networkId, rchainPnpDEFAULT_NETWORK_ID);
+    assert.equal(parsedOpts.rNode, rchainPnp.DEFAULT_RCHAIN_NODE);
+    assert.equal(parsedOpts.networkId, rchainPnp.DEFAULT_NETWORK_ID);
   });
 });
 
 describe('#getPluginDefinition()', function(){
   it('should have network, version and package name', function() {
-    var pluginDefinition = rchainPnpgetPluginDefinition();
+    var pluginDefinition = rchainPnp.getPluginDefinition();
 
-    assert.equal(rchainPnpNETWORK_NAME, pluginDefinition.network);
+    assert.equal(rchainPnp.NETWORK_NAME, pluginDefinition.network);
     assert.equal(packageData.name, pluginDefinition.package_name);
     assert.equal(packageData.version, pluginDefinition.version);
   });
@@ -51,6 +47,7 @@ describe('#getPluginDefinition()', function(){
 describe('#submitTransaction()', function() {
 
   it('should submit a valid transaction', function() {
+    const clock = () => new Date();
     const term = 'new test in { contract test(return) = { return!("test") } }';
     const timestamp = clock().valueOf();
 
@@ -63,11 +60,13 @@ describe('#submitTransaction()', function() {
   });
 
   it('should return error response for an invalid transaction', function() {
+    const clock = () => new Date();
     const term = 'new test in } contract test(return) = { return!("test") } {';
     const timestamp = clock().valueOf();
 
     rchainPnp.submitTransaction(term, rchainPnpOpts).then(function(pocketTxResponse) {
       assert.equal(pocketTxResponse.error, true);
+      assert.done();
     }, function() {
       console.error(arguments);
     });
